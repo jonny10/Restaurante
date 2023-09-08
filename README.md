a1f0e6a9835af5e148117b4408321e3ed2322094
Jonny10 <araujolopes2776@gmail.com>
2023-09-08 18:39:21 -0300

    Banco_de_dados_finalizado

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ Repositório de um site de um Restaurante/Bar com função de sacola, login e fu
 -05/09/2023: Finalizado o design do site, segue o link https://www.figma.com/community/file/1280923173262694790
 eu iria fazer esse projeto utilizando PHP e o codeingniter, mas irei alterar para o Node js, então já que será
 necessário, irei iniciar hoje os estudo para essa nova linguagem.
-Vou começar inserindo as possiveis rotas e alguns htmls
--06/09/2023: Bom dia bom dia, já inciei o dia assistindo mais algumas aulas então hoje irei adicionar um gitignore para ingnorar algumas partes do node na hora de subir para o github, e desenhar como será o banco de dados, terminei o desenho e adicinei aqui no projeto, agora estou caçando um meio de subir o banco em um servidor ou no localhost,
+Vou começar inserindo as possiveis rotas e alguns htmls.
+
+-06/09/2023: Bom dia bom dia, já inciei o dia assistindo mais algumas aulas então hoje irei adicionar um gitignore para ingnorar algumas partes do node na hora de subir para o github, e desenhar como será o banco de dados, terminei o desenho e adicinei aqui no projeto, agora estou caçando um meio de subir o banco em um servidor ou no localhost.
+
+-08/09/2023: Dia, da ultima vez comecei a construir os models de como seria as tabelas do banco de dados e já criando ela, para me acostumar com os códigos do sequelize, e hoje dando continuidade aos estudos, já criei todas as tabelas do banco, e agora adicionarei algumas informações inicias no banco, finalizei mais alguns estudo, e quando eu voltar, criarei as páginas em html.
--- banco_de_dados/tabelas.js
+++ banco_de_dados/tabelas.js
@@ -1,10 +1,5 @@
-const Sequelize = require('sequelize')
 const DataTypes = require('sequelize')
-const banco = new Sequelize('railway','root', 'fTkhW48P9Gox2yuqQbXV', {
-    host: 'containers-us-west-185.railway.app',
-    dialect: 'mysql',
-    port: '7153'
-})
+const banco = require("./conexao_bd")
 
 banco.authenticate().then(function(){
     console.log("sucesso paizão")
@@ -49,13 +44,15 @@ const usuario = banco.define('usuario', {
     },
     nome: {
         type: DataTypes.STRING,
+        allowNull: true
     },
     email: {
-        type: DataTypes.email,
-        unique: true
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: true
     },
     cep: {
-        type: DataTypes.INTEGER(8),
+        type: DataTypes.INTEGER(8)
     },
     complemento: {
         type: DataTypes.STRING
@@ -64,8 +61,167 @@ const usuario = banco.define('usuario', {
         type: DataTypes.STRING(11)
     },
     senha: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+})
+
+/*TABELA STATUS DO PEDIDO */
+const status_do_pedido = banco.define('status_do_pedido', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    status: {
         type: DataTypes.STRING
     }
 })
 
-usuario.sync({force: true})
+/*TABELA PEDIDOS */
+const pedidos = banco.define('pedidos', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    usuario_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: usuario,
+            key: 'id'
+        }
+    },
+    status_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: status_do_pedido,
+            key: 'id'
+        }
+    },
+    tempo_de_entrega: {
+        type: DataTypes.TIME  
+    }
+})
+
+/*TABELA TIPOS */
+const tipo = banco.define('tipo', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    tipo: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: true
+    }
+})
+
+/*TABELA TAMANHOS */
+const tamanho = banco.define('tamanho', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    tamanho: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: true
+    }
+})
+
+/*TABELA ITEM */
+const item = banco.define('item', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    titulo: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: true
+    },
+    descricao: {
+        type: DataTypes.TEXT
+    },
+    tamanho_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: tamanho,
+            key: 'id'
+        }
+    },
+    tipo_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: tipo,
+            key: 'id'
+        }
+    },
+    imagem_do_item: {
+        type: DataTypes.BLOB('medium')
+    },
+    valor: {
+        type: DataTypes.DECIMAL(10,2),
+        allowNull: true
+    }
+})
+
+/*TABELA DE JUNÇÃO PEDIDOS_ITEM */
+const pedidos_itens = banco.define('pedidos_itens', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    pedidos_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: usuario,
+            key: 'id'
+        }
+    },
+    item_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: item,
+            key: 'id'
+        }
+    },
+    quantidade: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    valor_item: {
+        type: DataTypes.DECIMAL(10,2),
+        allowNull: true
+    },
+    valor_total: {
+        type: DataTypes.DECIMAL(10,2),
+        allowNull: true
+    }
+})
+
+module.exports = {
+    cronograma,
+    usuario,
+    status_do_pedido,
+    pedidos,
+    tipo,
+    tamanho,
+    item,
+    pedidos_itens
+}
+
+/** 
+ * sincronizar banco
+banco.sync({force: true})
+*/
